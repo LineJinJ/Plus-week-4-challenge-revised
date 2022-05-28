6cd7a07dd4498bfcb1efabe91a860c7334a77d7d
LineJinJ <102582136+LineJinJ@users.noreply.github.com>
2022-05-28 14:00:38 +0200

    console.log(hours, minutes);

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ let dateElement = document.querySelector("#date");
 let currentTime = new Date();
 let hours = currentTime.getHours();
 let minutes = currentTime.getMinutes();
+
+console.log(hours, minutes);
 dateElement.innerHTML = "Saturday 13:55 ";
