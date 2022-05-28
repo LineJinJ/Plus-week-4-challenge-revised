418b6e3ce89472f3bd95f4d6376f1cdacb62aba8
LineJinJ <102582136+LineJinJ@users.noreply.github.com>
2022-05-28 14:28:08 +0200

    Created function formatDate(date) Changed the currentTIme to date every where in the code

--- src/index.js
+++ src/index.js
@@ -1,27 +1,32 @@
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    `0${minutes}`;
+  }
+
+  let dayIndex = date.getDay();
+
+  //Arrays are [] and objects are {} that is the difference
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  return `${days[dayIndex]} ${hours}:${minutes}`;
+}
+
 let dateElement = document.querySelector("#date");
 let currentTime = new Date();
-let hours = currentTime.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
-}
-
-let minutes = currentTime.getMinutes();
-if (minutes < 10) {
-  `0${minutes}`;
-}
-
-let dayIndex = currentTime.getDay();
-
-//Arrays are [] and objects are {} that is the difference
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
 
 console.log(days[dayIndex]);
 
