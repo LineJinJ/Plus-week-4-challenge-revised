fdf09938f788913cfa8e99a5ca4a84528a875012
LineJinJ <102582136+LineJinJ@users.noreply.github.com>
2022-05-28 15:30:34 +0200

    Convert To Celsius and create link --> At this stage has all coding on page but now we are going to simplify the code in many steps

--- src/index.js
+++ src/index.js
@@ -55,3 +55,5 @@ searchForm.addEventListener("submit", search);
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", convertToFahrenheit);
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", convertToCelsius);
