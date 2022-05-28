7a6df93718514d08198fb13ea69afd9768e48680
LineJinJ <102582136+LineJinJ@users.noreply.github.com>
2022-05-28 14:21:54 +0200

    Fixed the minutes so i shows -- and instead of - one number only

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 let dateElement = document.querySelector("#date");
 let currentTime = new Date();
 let hours = currentTime.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+
 let minutes = currentTime.getMinutes();
+if (minutes < 10) {
+  `0${minutes}`;
+}
+
 let dayIndex = currentTime.getDay();
 
 //Arrays are [] and objects are {} that is the difference
