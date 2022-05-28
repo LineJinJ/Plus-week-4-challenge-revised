ed0b953d78784eca8667297abcf3bf8769377f9d
LineJinJ <102582136+LineJinJ@users.noreply.github.com>
2022-05-28 14:05:40 +0200

    Added let day and it shows 6 which means it is Saturday - will fix this to show a day and not number

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ let dateElement = document.querySelector("#date");
 let currentTime = new Date();
 let hours = currentTime.getHours();
 let minutes = currentTime.getMinutes();
+let day = currentTime.getDay();
+
+console.log(day);
 
 dateElement.innerHTML = `Saturday ${hours}:${minutes}`;
