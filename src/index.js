b7cde04b452039c5fb0ee6b7dfe89ef1ca27c6ff
LineJinJ <102582136+LineJinJ@users.noreply.github.com>
2022-05-28 14:12:27 +0200

    Included the array [from sunday to saturday] let days...

--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ let hours = currentTime.getHours();
 let minutes = currentTime.getMinutes();
 let day = currentTime.getDay();
 
+//Arrays are [] and objects are {} that is the difference
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
 console.log(day);
 
 dateElement.innerHTML = `Saturday ${hours}:${minutes}`;
