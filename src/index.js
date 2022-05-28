962136cdaf8ca1ef2ed928b04e073f8f2c304c84
LineJinJ <102582136+LineJinJ@users.noreply.github.com>
2022-05-28 15:12:09 +0200

    Needed to go back in my code. Was not following how to do the bonus point

--- src/index.js
+++ src/index.js
@@ -34,10 +34,17 @@ function search(event) {
   cityElement.innerHTML = cityInput.value;
 }
 
+function convertToFahreinheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = temperatureElement.innerHTML;
+  temperatureElement.innerHTML = (temperature0°C * 9)/5 +32;
+}
+
 //Feature 1#
 let dateElement = document.querySelector("#date");
 let currentTime = new Date();
-dateElement.innerHTML = formatDate(currentTime);
+dateElement.innerHTML = formatDate(date);
 
 //Feature #2
 let searchForm = document.querySelector("#search-form");
@@ -45,5 +52,5 @@ searchForm.addEventListener("submit", search);
 
 //Feature #3 Bonus point
 
-let temperatureElement = document.querySelector("#temperature");
-let temperature = temperatureElement.innerHTML;
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", convertToFahreinheit);
